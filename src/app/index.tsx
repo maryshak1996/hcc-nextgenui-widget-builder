ce2d04a2d61efd9e65957e3610330ade753067ec
Developer <developer@redhat.com>
2026-02-16 11:06:32 -0500

    Remove GH Pages basename for Vercel

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ import { AppRoutes } from '@app/routes';
 import '@app/app.css';
 
 const App: React.FunctionComponent = () => (
-  <Router basename={process.env.NODE_ENV === 'production' ? '/HCC-cursor-seed' : ''}>
+  <Router>
     <AppLayout>
       <AppRoutes />
     </AppLayout>
